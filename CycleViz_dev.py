1f887154d847dc9315b1cab25a84008dd8bab1f4
Jens Luebeck <jluebeck@eng.ucsd.edu>
2019-04-09 15:01:15 -0700

    bugfix to imports

--- CycleViz_dev.py
+++ CycleViz_dev.py
@@ -11,8 +11,9 @@ from matplotlib import pyplot as plt
 import matplotlib.patches as mpatches
 from matplotlib import colors as mcolors
 import matplotlib._color_data as mcd
-from matplotlib.collections import PatchCollection
-from matplotlib.font_manager import FontProperties
+import matplotlib
+# from matplotlib.collections import PatchCollection
+# from matplotlib.font_manager import FontProperties
 from intervaltree import Interval, IntervalTree
 
 #non-interactive backend
